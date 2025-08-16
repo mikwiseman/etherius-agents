9bd05251d9d9c25dda1b77615b5afcd7c7ab1d77
Mik Wiseman <hi@mikwiseman.com>
2025-08-15 20:40:59 -0400

    Agent added

---
+++ mailbox_agent.py
@@ -0,0 +1,19 @@
+from uagents import Agent, Context, Model
+ 
+class Message(Model):
+    message: str
+ 
+SEED_PHRASE = "mailbox282828"
+ 
+# Now your agent is ready to join the Agentverse!
+agent = Agent(
+    name="mailbox282828",
+    port=8000,
+    mailbox=True        
+)
+ 
+# Copy the address shown below
+print(f"Your agent's address is: {agent.address}")
+ 
+if __name__ == "__main__":
+    agent.run()
